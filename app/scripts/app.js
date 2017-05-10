3c6190cf2bf9de9d6fd1aba78326c3eb042b2ba8
zhulinlin12 <2638872000@qq.com>
2017-05-10 19:12:47 +0800

    Signed-off-by: zhulinlin12 <2638872000@qq.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,13 +10,27 @@
  */
 angular
   .module('yomApp', ["ui.router",'oc.lazyLoad'])
-  .config(["$stateProvider","$urlRouterProvider",'$httpProvider',function($stateProvider,$urlRouterProvider,$httpProvider){
+  .config(["$stateProvider","$urlRouterProvider",'$httpProvider','$ocLazyLoadProvider',function($stateProvider,$urlRouterProvider,$httpProvider,$ocLazyLoadProvider){
   	//延迟加载
 var lazyloadlib = [];
 
   	$stateProvider.state("index",{ 
        url:"/toubu", 
        templateUrl:'views/shouye.html'
+//     resolve: {  
+//          des: function ($ocLazyLoad) {  
+//              //var ctrl = $ocLazyLoad.load('/scripts/myjs/home.js');  
+//              var ctrl = $ocLazyLoad.load([  
+//                  {  
+//                      files: '/scripts/zll/toubu.js',  
+//                      cache: true //指定是否启用缓存  
+//                  }  
+//              ]);  
+//              if (ctrl.$$state.status == 2)  
+//                  return undefined;  
+//              return ctrl;  
+//          }  
+//      }  
      
      }).state('index.op',{
 			url:'/synr',
@@ -52,8 +66,37 @@ var lazyloadlib = [];
 			'a@index.pj':{templateUrl:'views/sppj.html'},
 			'b@index.pj':{templateUrl:'views/dibu.html'}
 			}
-		})
+		}).state('index.buy',{
+			url:'/buy',
+			views:{
+			'':{templateUrl:'views/heye.html'},
+			'a@index.buy':{templateUrl:'views/buy.html'},
+			'b@index.buy':{templateUrl:'views/dibu.html'}
+			}
+		}).state('index.xq',{
+			url:'/xq',
+			views:{
+			'':{templateUrl:'views/heye.html'},
+			'a@index.xq':{templateUrl:'views/spxq.html'},
+			'b@index.xq':{templateUrl:'views/dibu.html'}
+			}
+		}).state('index.order',{
+			url:'/order',
+			views:{
+			'':{templateUrl:'views/heye.html'},
+			'a@index.order':{templateUrl:'views/order.html'},
+			'b@index.order':{templateUrl:'views/dibu.html'}
+			}
+		}).state("login",{ 
+       url:"/login", 
+       templateUrl:'views/login.html'     
+     })
   	
-  	$urlRouterProvider.when("","/toubu/synr");
+  	$urlRouterProvider.when("","/login");
+//	 //全局配置  
+//  $ocLazyLoadProvider.config({  
+//      debug: true, //知否启用调试模式  
+//      events:true  //事件绑定是否启用  
+//  });  
   }])
 
